ebf745447116cf5f09b71c3d2b1d20bd7362d0ca
sabrina553 <sabrinalily@pm.me>
2025-10-15 16:56:32 +0100

    reverse-words

--- python/7kyu/reverse-words.py
+++ python/7kyu/reverse-words.py
@@ -1,13 +1,16 @@
 '''starting to just google things more blatantly rather than techniques, i suppose it\'s still learning '''
 def reverse_words(text):
     a = []
-    text = text.split()
+    text = text.split(' ')
     for i in text: 
         a.append(i[::-1])
     return ' '.join(a)
 print(reverse_words("hi my name is sabrina"))
 
-#proud of this one :) 
+#proud of this one :)
 def better(text):
-    return ' '.join(i[::-1] for i in text.split())
-print(reverse_words("hewwo my name is"))
+    return ' '.join(i[::-1] for i in text.split(' '))
+print(better("hi  my  name  is"))
+#nvm it doesn't work for double spaces :)
+# had to add (' ') to the split function :) 
+
